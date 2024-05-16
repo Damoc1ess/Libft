408617926713e74bc64e1db04c10b31ee0cb723c
Florent Flamion <fflamion@bess-f2r3s10.clusters.42paris.fr>
2024-05-16 15:26:22 +0200

    test

--- Makefile
+++ Makefile
@@ -1,4 +1,28 @@
-NAME = libft.a
-CC = gcc
-CFLAGS = -Wall -Wextra -Werror
-RM = rm -f
+NAME		= libft.a
+SRCS		= srcs/ft_isalpha.c\
+				srcs/ft_isdigit.c\
+
+			
+OBJS		= $(SRCS:.c=.o)
+INCLUDES	= includes
+FLAGS 		= -Werror -Wextra -Wall
+CC 		= gcc
+
+all:	$(NAME)
+
+$(NAME): $(OBJS)
+	ar rc $(NAME) $(OBJS)
+	ranlib $(NAME)
+
+.c.o:
+	$(CC) $(FLAGS) -I $(INCLUDES) -c $< -o $(@:.c=.o)
+
+clean: 
+	rm -f $(OBJ)
+
+fclean: clean
+	rm -f $(NAME)
+
+re:	fclean all
+
+.PHONY: all clean fclean re
--- ft_atoi.c
+++ ft_atoi.c
@@ -6,19 +6,19 @@
 /*   By: fflamion <fflamion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 10:46:36 by fflamion          #+#    #+#             */
-/*   Updated: 2024/05/16 11:34:15 by fflamion         ###   ########.fr       */
+/*   Updated: 2024/05/16 14:48:00 by fflamion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi (const char *str)
+int	ft_atoi(const char *str)
 {
-	int	neg;
+	int		neg;
 	long	nbr;
 
 	neg = 1;
-	while ((*str == " " )||(*str <= 9 && *str >= 13))
+	while ((*str == " " ) || (*str <= 9 && *str >= 13))
 		*str++;
 	while (*str == "-" || *str == "+")
 	{
@@ -31,5 +31,4 @@ int	ft_atoi (const char *str)
 	while (*str && ft_isdigit(*str))
 		nbr = nbr * 10 + (*str - 48);
 	return (nbr * neg);
-
 }
